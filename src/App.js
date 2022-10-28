94743672e0d4b823ddf63de3fc0ae68be2fb55e2
Ananya Singhal <reach.ananya.singhal@gmail.com>
2022-10-28 21:39:44 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SortViz from './SortViz/SortViz'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SortViz>
+         
+      </SortViz>
+    </div>
+  );
+}
+
+export default App;
